e2b54719cdebeaef422551a9fa8cc822bcbb2c95
daipeijia <dea9703@163.com>
2022-09-24 15:00:25 +0800

    去除严格模式节点

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import App from './App';  /** 引入根组件 */
 // 渲染根组件App 到一个id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // React.StrictMode: React的严格模式节点。需要去掉，会影响useEffect的执行时机
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
