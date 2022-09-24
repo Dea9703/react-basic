b23323ed5160d4ca8f2520b07448cc61363cc672
daipeijia <dea9703@163.com>
2022-09-24 14:57:37 +0800

    认识index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'; /** React：框架核心包 */
+import ReactDOM from 'react-dom/client'; /** ReactDOM：专门做渲染相关的包 */
+import './index.css'; /** 应用的全局样式文件 */
+import App from './App';  /** 引入根组件 */
 
-
+// 渲染根组件App 到一个id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
