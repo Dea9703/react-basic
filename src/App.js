15d8a35f8d8d94c7992a22deeb1c0b52eba38e21
daipeijia <dea9703@163.com>
2022-10-16 14:18:10 +0800

    基础完结

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
-      <main>App</main>
-    </div>
-  );
+import React, { useContext, createContext } from 'react'
+
+const Context = createContext()
+
+function Foo () {
+  return (<div>
+    <div>this is Foo</div>
+    <Bar />
+  </div>)
 }
 
-export default App;
+function Bar () {
+  // 底层组件通过useContext函数获取数据  
+  const name = useContext(Context)
+  return <div>this is Bar name: {name}</div>
+}
+
+function App () {
+  return (
+    // 顶层组件通过Provider 提供数据    
+    <Context.Provider value={'this is name'}>
+      <div><Foo /></div>
+    </Context.Provider>
+  )
+}
+
+export default App
